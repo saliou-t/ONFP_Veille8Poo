073cc5cf75fed3520caff4e6f995809d7e50fdca
saliou-t <zalltalla46@gmail.com>
2021-06-27 10:31:39 +0000

    les constructeurs

---
+++ Vehicule.php
@@ -0,0 +1,31 @@
+<?php
+class Vehicule{
+
+    public function __construct($marque, $matricule, $couleur, $vitesse){
+        $this->marque = $marque;
+        $this->matricule = $matricule;
+        $this->couleur = $couleur;
+        $this->vitesse = $vitesse;
+    }
+
+    public function rouler(){
+        var_dump("La voiture de marque $this->marque de couleur $this->couleur roule à $this->vitesse à l'heure");
+    }
+    /**
+     * Un setteur qui prends un paramètre et permet de modifier la couleur d'un vehicule par exemple
+     */
+    public function setColor($newColor){
+        $this->couleur = $newColor;
+    }
+
+    /**
+     *Un getteur qui permet d'obtenir la marque d'un vehicule par exemple 
+     */
+    public function getMarque(){
+        return $this->marque;
+    }
+}
+$voiture = new Vehicule("Renault","DK-0256-OF","Gris",310);
+
+var_dump($voiture->getMarque());
+
---
+++ voiture.php
@@ -0,0 +1,22 @@
+<?php
+
+require('Vehicule.php');
+
+class Voiture extends Vehicule{
+
+    public function __construct($marque, $matricule, $couleur, $vitesse,$fuel){
+        
+        parent::__construct($marque, $matricule, $couleur, $vitesse); 
+        $this->fuel = $fuel; //une proprièté supplé de la classe "voiture"
+    }
+
+    //une fonction propre à la classe Voiture
+    public function rouler2(){
+    	var_dump("La voiture $this->marque consomme du $this->fuel");
+    }
+}
+
+$voiture1 = new Voiture("Porche","DK-7477-OF","Noire Mat",330,"gazoil");
+$voiture1->rouler();
+$voiture1->rouler2();
+
